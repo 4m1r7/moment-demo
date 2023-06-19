95adfec0a11c677d511688f83849bddeca11b565
Amir <amir@Amirs-MacBook-Air.local>
2023-06-20 01:36:22 +0330

    chore: initial Moment push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,85 @@
+import { motion } from 'framer-motion';
 import * as React from 'react';
+import { useState } from 'react';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
 import Seo from '@/components/Seo';
-
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Vercel from '~/svg/Vercel.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
 
+interface Position {
+  x: string;
+  y: string;
+}
+
+interface ShapePositions {
+  red: Position;
+  green: Position;
+  blue: Position;
+}
+
+// Define a constant for the initial positions
+const initialPositions: ShapePositions = {
+  red: { x: '5vw', y: '4vh' },
+  green: { x: '10vw', y: '10vh' },
+  blue: { x: '13vw', y: '9vh' },
+};
+
+const shapePositions: { [key: string]: ShapePositions } = {
+  red: {
+    red: { x: '36vw', y: '7vh' },
+    green: { x: '64vw', y: '1vh' },
+    blue: { x: '88vw', y: '5vh' },
+  },
+  green: {
+    red: { x: '73vw', y: '15vh' },
+    green: { x: '100vw', y: '13vh' },
+    blue: { x: '47vw', y: '17vh' },
+  },
+  blue: {
+    red: { x: '15vw', y: '3vh' },
+    green: { x: '58vw', y: '11vh' },
+    blue: { x: '13vw', y: '9vh' },
+  },
+};
+
 export default function HomePage() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [positions, setPositions] = useState<any>(initialPositions);
+
+  const handleShapeClick = (shape: string) => {
+    setPositions(shapePositions[shape]);
+  };
+
+  // Create a function to render the shapes, this will help when adding more shapes
+  const renderShape = (color: string) => (
+    <motion.svg
+      key={color}
+      initial='initial'
+      animate={positions[color]}
+      width='100'
+      height='100'
+      style={{ position: 'absolute' }} // Use absolute positioning for the shapes
+      onClick={() => handleShapeClick(color)}
+    >
+      fe
+      <rect
+        fill={color}
+        width='100'
+        height='100'
+        style={{
+          rotate: positions[color].rotate,
+          scale: positions[color].scale,
+        }}
+      />
+    </motion.svg>
+  );
+
+  const shapeColors = ['red', 'green', 'blue'];
+
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
@@ -28,42 +87,15 @@ export default function HomePage() {
 
       <main>
         <section className='bg-white'>
-          <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
-
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
-
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
-              />
-            </UnstyledLink>
+          <div className='layout relative flex min-h-screen flex-col items-center justify-center text-center'>
+            <div className='relative m-0 h-screen w-screen bg-red-200 p-0'>
+              {shapeColors.map((color) => renderShape(color))}
+            </div>
 
             <footer className='absolute bottom-2 text-gray-700'>
               © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
+              <UnderlineLink href='mailto:taheri.amirhossein@gmail.com'>
+                AmirT
               </UnderlineLink>
             </footer>
           </div>
