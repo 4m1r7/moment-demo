cc12eb7ce00a75dbd58bd05e0debb9e0d71ae245
Amir Taheri <AmirTaheri@me.com>
2024-02-29 12:53:15 +0330

    Update apolloClient.ts

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://cms.moment-studio.ir/graphql',
+  uri: 'https://cms.moment-studio.ir/graphql',
   cache: new InMemoryCache(),
 });
 
