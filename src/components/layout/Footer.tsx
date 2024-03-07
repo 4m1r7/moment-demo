ba4680fd358412e1b5b2a31f39437d1469febc0e
Amir <amir@Amirs-MacBook-Air.local>
2024-03-07 18:01:35 +0330

    chore(footer): added instagram logo in the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,21 +1,40 @@
 import * as React from 'react';
 
+import Instagram from '~/svg/instagram.svg';
+
 export default function Footer() {
   return (
     <footer className='text-customGray flex h-fit w-full flex-col justify-center px-32 py-52 text-7xl font-light md:py-16 md:text-3xl '>
-      <div className='border-customGray flex w-full justify-between gap-28 border-t py-20 md:hidden'>
-        <div className='aspect-square h-48 rounded-full bg-stone-200' />
-        <div className='aspect-square h-48 rounded-full bg-stone-200' />
-        <div className='aspect-square h-48 rounded-full bg-stone-200' />
+      <div className='border-customGray flex w-full justify-center gap-28 border-t py-20 md:hidden'>
+        <a href='https://www.instagram.com/momentstudio.arch/' target='_blank'>
+          <Instagram
+            src='/instagram.svg'
+            className='h-24 w-24'
+            style={{
+              filter:
+                'brightness(0) saturate(100%) invert(54%) sepia(0%) saturate(0%) hue-rotate(225deg) brightness(92%) contrast(89%)',
+            }}
+          />
+        </a>
       </div>
 
       <div className='border-customGray relative flex w-full justify-between border-b border-t py-9'>
         <p>Designed @ [ BÂZ.SPACE ]</p>
 
         <div className='absolute hidden w-full justify-center gap-8 md:flex'>
-          <div className='aspect-square h-10 rounded-full bg-stone-200' />
-          <div className='aspect-square h-10 rounded-full bg-stone-200' />
-          <div className='aspect-square h-10 rounded-full bg-stone-200' />
+          <a
+            href='https://www.instagram.com/momentstudio.arch/'
+            target='_blank'
+          >
+            <Instagram
+              src='/instagram.svg'
+              className='h-8 w-8'
+              style={{
+                filter:
+                  'brightness(0) saturate(100%) invert(54%) sepia(0%) saturate(0%) hue-rotate(225deg) brightness(92%) contrast(89%)',
+              }}
+            />
+          </a>
         </div>
 
         <p>All Rights Reserved.</p>
